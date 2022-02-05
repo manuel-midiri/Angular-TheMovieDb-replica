6cbf76dfa6a22f4a2b585de852de3adca6107417
Manuel Midiri <manuel.midiri@hotmail.it>
2022-02-05 17:58:18 +0100

    minor fix

--- src/app/tv/tv.component.ts
+++ src/app/tv/tv.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { SharedService } from '../services/shared.service';
+import { TheMovieDBService } from '../services/theMovieDB.service';
+import { UpdateBSMovieService } from '../services/updateBSMovie/updateBSMovie.service';
 
 @Component({
   selector: 'app-tv',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TvComponent implements OnInit {
 
-  constructor() { }
+  cardArray: any;
+  btnVisible: boolean = true;
+
+  constructor(
+    private theMovieDbService: TheMovieDBService,
+    private router: Router,
+    private sharedSservice: SharedService,
+    private updateBSservice: UpdateBSMovieService
+  ) { }
 
   ngOnInit() {
-  }
+    this.sharedSservice.othersChargeBS.next(false);
+
+    this.theMovieDbService.moviePopular$.subscribe(movies => this.cardArray = movies);
+    this.router.events.pipe(
+      filter((navigation): navigation is NavigationEnd => navigation instanceof NavigationEnd),
+      ).subscribe(() => this.btnVisible = true);
+    this.sharedSservice.othersCharge$.subscribe(valueBoolean => {
+      if (valueBoolean) {
+        this.updateBSservice.detectingChange();
+      } else {
+        this.updateBSservice.getMovie();
+      }
+    });
+    }
+
+    onClickedButton(){
+      this.btnVisible = false;
+      this.sharedSservice.othersChargeBS.next(true);
+    }
 
 }
